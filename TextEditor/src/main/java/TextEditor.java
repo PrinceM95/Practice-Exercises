3ca6221dfd3fa480a11029f628b5468577e1c174
PrinceM95 <moutimap@gmail.com>
2023-05-15 19:30:15 -0400

    worked on completing fontSizeSpinner feature and added fontLabel

--- TextEditor/src/main/java/TextEditor.java
+++ TextEditor/src/main/java/TextEditor.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,6 +10,7 @@ public class TextEditor extends JFrame implements ActionListener {
     //Declaration of an instance variable
     JTextArea textArea;
     JScrollPane scrollPane;
+    JLabel fontLabel;
     JSpinner fontSizeSpinner;
 
     //Constructor
@@ -27,10 +30,24 @@ public class TextEditor extends JFrame implements ActionListener {
         scrollPane.setPreferredSize(new Dimension(450, 450));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
+        fontLabel = new JLabel("Font: ");
+
+        fontSizeSpinner = new JSpinner();
+        fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
+        fontSizeSpinner.setValue(20);
+        fontSizeSpinner.addChangeListener(new ChangeListener() {
+
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                textArea.setFont(new Font(textArea.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+            }
+        });
+        this.add(fontLabel);
+        this.add(fontSizeSpinner);
         this.add(scrollPane);
         this.setVisible(true);
-
     }
+
 
     @Override
     //When an action event occurs, such as a button click, the actionPerformed() method is invoked, and any code within it is executed.
