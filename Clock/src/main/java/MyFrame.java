9932d4c1eb4100b1afe08ae5e86f9328f9c35cf1
PrinceM95 <moutimap@gmail.com>
2023-05-28 10:40:26 -0400

    calendar and constructor included

--- Clock/src/main/java/MyFrame.java
+++ Clock/src/main/java/MyFrame.java
@@ -1,5 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MyFrame extends JFrame {
-    fdfd
+
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Clock Program");
+        this.setLayout(new FlowLayout());
+        this.setSize(350,200);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("");
+        this.setVisible(true);
+    }
+
 }
